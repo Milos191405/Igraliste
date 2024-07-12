5daeff3991f3eaab1963ad9c253a47c8cd858b09
Milos Mirkovic <milos.mirkovic7@gmail.com>
2024-07-12 23:11:42 +0200

    fixing welcome page

--- Igraliste/src/components/Dobrodosli.jsx
+++ Igraliste/src/components/Dobrodosli.jsx
@@ -5,11 +5,11 @@ import Logo from "../assets/Logo.jpeg";
 function Dobrodosli() {
   return (
     <div className="fixed inset-0 flex flex-col justify-center items-center bg-white">
-      <img src={Dobrodosli1} alt="Dobrodosli u deciju igraonicu" className="" />
+      <img src={Dobrodosli1} alt="Dobrodosli u deciju igraonicu" className="xl:w-[600px] py-10" />
       <p className="text-center text-3xl font-style: italic">
-        u
+        
       </p>
-      <img src={Logo} alt="" />
+      <img src={Logo} alt="" className="xl:w-[600px]" />
     </div>
   );
 }
