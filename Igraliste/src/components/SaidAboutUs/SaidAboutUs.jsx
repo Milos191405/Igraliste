215326e7a332fd946edf4adea8ce1be11ce25c00
Milos Mirkovic <milos.mirkovic7@gmail.com>
2024-08-16 23:32:02 +0200

    fixing carousel css

--- Igraliste/src/components/SaidAboutUs/SaidAboutUs.jsx
+++ Igraliste/src/components/SaidAboutUs/SaidAboutUs.jsx
@@ -5,7 +5,7 @@ import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 // Import Swiper styles
 //import "swiper/css";
 //import "swiper/css/pagination";
-//import "./styles.css";
+import "./styles.css";
 // Import required modules
 // import { Pagination, Autoplay } from "swiper/modules";
 export default function App() {
